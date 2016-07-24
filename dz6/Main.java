ced475a49f97cdc8d7977eca1fd7998e5a5d6f07
Yurok-vrs <yurok-vrs@mail.ru>
2016-07-24 12:57:35 +0300

    Создан основной клас для приложения "Чат"

---
+++ dz6/Main.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.java2.dz.dz6.ЮрийНиконоров;
+
+import ru.geekbrains.java2.dz.dz6.ЮрийНиконоров.swing.ChatWindow;
+
+/**
+ * Created by Acer on 19.06.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ChatWindow chatWindow = new ChatWindow();
+
+
+    }
+}
